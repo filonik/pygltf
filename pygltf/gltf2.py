a65bc7037604a840d0641d43e6999b705522a4b3
filonik <filonik@users.noreply.github.com>
2019-03-23 19:04:50 +1100

    Added textures.

--- pygltf/gltf2.py
+++ pygltf/gltf2.py
@@ -19,10 +19,14 @@ class Uniform(enum.Enum):
 class Attribute(enum.Enum):
     POSITION   = "POSITION"
     NORMAL     = "NORMAL"
+    BINORMAL   = "BINORMAL"
+    TANGENT    = "TANGENT"
     TEXCOORD   = "TEXCOORD"
     TEXCOORD_0 = "TEXCOORD_0"
     TEXCOORD_1 = "TEXCOORD_1"
+    COLOR      = "COLOR"
     COLOR_0    = "COLOR_0"
+    COLOR_1    = "COLOR_1"
     JOINTS_0   = "JOINTS_0"
     WEIGHTS_0  = "WEIGHTS_0"
     @staticmethod
@@ -69,6 +73,25 @@ class PrimitiveMode(enum.Enum):
     TRIANGLE_STRIP = 5
     TRIANGLE_FAN   = 6
 
+class AlphaMode(enum.Enum):
+    OPAQUE = "OPAQUE"
+    MASK   = "MASK" 
+    BLEND  = "BLEND"
+
+class Filter(enum.Enum):
+    NEAREST = 9728 
+    LINEAR = 9729
+    NEAREST_MIPMAP_NEAREST = 9984
+    LINEAR_MIPMAP_NEAREST = 9985
+    NEAREST_MIPMAP_LINEAR = 9986
+    LINEAR_MIPMAP_LINEAR = 9987
+
+class Wrap(enum.Enum):
+    CLAMP_TO_EDGE = 33071
+    MIRRORED_REPEAT = 33648 
+    REPEAT = 10497
+
+
 def askey(name):
     return name.replace(" ", "_").lower()
 
@@ -87,6 +110,7 @@ class Object(object):
         if self.extras:
             result["extras"] = self.extras
         return result
+
 
 class Document(object):
     @classmethod
@@ -116,7 +140,7 @@ class Document(object):
         self.accessors    = []
         self.animations   = []
         self.buffers      = []
-        self.buffer_views = []
+        self.bufferViews  = []
         self.cameras      = []
         self.images       = []
         self.materials    = []
@@ -124,12 +148,14 @@ class Document(object):
         self.nodes        = []
         self.samplers     = []
         self.scenes       = []
+        self.skins        = []
+        self.textures     = []
         self.scene        = kwargs.get('scene', None)
         
         self.add_accessors(kwargs.get('accessors', []))
         self.add_animations(kwargs.get('animations', []))
         self.add_buffers(kwargs.get('buffers', []))
-        self.add_buffer_views(kwargs.get('buffer_views', []))
+        self.add_buffer_views(kwargs.get('bufferViews', []))
         self.add_cameras(kwargs.get('cameras', []))
         self.add_images(kwargs.get('images', []))
         self.add_materials(kwargs.get('materials', []))
@@ -137,6 +163,8 @@ class Document(object):
         self.add_nodes(kwargs.get('nodes', []))
         self.add_samplers(kwargs.get('samplers', []))
         self.add_scenes(kwargs.get('scenes', []))
+        self.add_skins(kwargs.get('skins', []))
+        self.add_textures(kwargs.get('textures', []))
     
     def add_accessor(self, value):
         value.key = len(self.accessors)
@@ -148,8 +176,8 @@ class Document(object):
         value.key = len(self.buffers)
         self.buffers.insert(value.key, value)
     def add_buffer_view(self, value):
-        value.key = len(self.buffer_views)
-        self.buffer_views.insert(value.key, value)
+        value.key = len(self.bufferViews)
+        self.bufferViews.insert(value.key, value)
     def add_camera(self, value):
         value.key = len(self.cameras)
         self.cameras.insert(value.key, value)
@@ -171,6 +199,12 @@ class Document(object):
     def add_scene(self, value):
         value.key = len(self.scenes)
         self.scenes.insert(value.key, value)
+    def add_skin(self, value):
+        value.key = len(self.skins)
+        self.skins.insert(value.key, value)
+    def add_texture(self, value):
+        value.key = len(self.textures)
+        self.textures.insert(value.key, value)
     
     def add_accessors(self, values):
         for value in values:
@@ -205,14 +239,20 @@ class Document(object):
     def add_scenes(self, values):
         for value in values:
             self.add_scene(value)
+    def add_skins(self, values):
+        for value in values:
+            self.add_skin(value)
+    def add_textures(self, values):
+        for value in values:
+            self.add_texture(value)
     
     def togltf(self):
         result = {}
         result["asset"] = self.asset
         if self.buffers:
             result["buffers"]     = [buffer.togltf()      for buffer      in self.buffers]
-        if self.buffer_views:
-            result["bufferViews"] = [buffer_view.togltf() for buffer_view in self.buffer_views]
+        if self.bufferViews:
+            result["bufferViews"] = [bufferView.togltf()  for bufferView  in self.bufferViews]
         if self.accessors:
             result["accessors"]   = [accessor.togltf()    for accessor    in self.accessors]
         if self.animations:
@@ -231,19 +271,227 @@ class Document(object):
             result["samplers"]    = [sampler.togltf()     for sampler     in self.samplers]
         if self.scenes:
             result["scenes"]      = [scene.togltf()       for scene       in self.scenes]
+        if self.skins:
+            result["skins"]       = [skin.togltf()        for skin        in self.skins]
+        if self.textures:
+            result["textures"]    = [texture.togltf()     for texture     in self.textures]
         if self.scene:
             result["scene"] = self.scene.key
         return result
 
-class Scene(Object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
-        self.key   = -1
-        self.nodes = kwargs.get('nodes', [])
+glTF = Document
+
+
+class Accessor(Object):
+    def __init__(self, bufferView, byteOffset=None, count=0, type=AccessorType.SCALAR, componentType=ComponentType.FLOAT, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key  = -1
+        self.bufferView = bufferView
+        self.byteOffset = byteOffset
+        self.componentType = componentType
+        self.normalized = False
+        self.count = count
+        self.type = type
+        self.max = None
+        self.min = None
     def togltf(self):
         result = super().togltf()
-        result["nodes"] = [node.key for node in self.nodes]
+        result["bufferView"] = self.bufferView.key
+        result["componentType"] = self.componentType.value
+        result["count"] = self.count
+        result["type"] = self.type.value
+        if self.bufferView:
+            result["bufferView"] = self.bufferView.key
+        if self.byteOffset is not None:
+            result["byteOffset"] = self.byteOffset
+        if self.max:
+            result["max"] = self.max
+        if self.min:
+            result["min"] = self.min
         return result
+
+
+#TODO
+class Sparse(Object):
+    pass
+
+Accessor.Sparse = Sparse
+
+
+#TODO
+class Animation(Object):
+    pass
+
+
+class Buffer(Object):
+    def __init__(self, byteLength=0, uri=None, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        self.key   = -1
+        self.byteLength = byteLength
+        self.uri = uri
+    def togltf(self):
+        result = super().togltf()
+        result["byteLength"] = self.byteLength
+        if self.uri:
+            result["uri"] =  self.uri
+        return result
+
+
+class BufferView(Object):
+    def __init__(self, buffer, byteOffset=None, byteLength=0, byteStride=None, target=BufferTarget.ARRAY_BUFFER, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key    = -1
+        self.buffer = buffer
+        self.byteOffset = byteOffset
+        self.byteLength = byteLength
+        self.byteStride = byteStride
+        self.target = target
+    def togltf(self):
+        result = super().togltf()
+        result["buffer"] = self.buffer.key
+        result["byteLength"] = self.byteLength
+        if self.byteOffset is not None:
+            result["byteOffset"] = self.byteOffset
+        if self.byteStride is not None:
+            result["byteStride"] = self.byteStride
+        if self.target:
+            result["target"] = self.target.value
+        return result
+
+
+#TODO
+class Camera(Object):
+    pass
+
+
+class Image(Object):
+    def __init__(self, uri=None, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        self.key  = -1
+        self.uri = uri
+        self.mimeType = kwargs.get("mimeType")
+        self.bufferView = kwargs.get("bufferView")
+        self.extras = kwargs.get("extras")
+    def togltf(self):
+        result = super().togltf()
+        if self.uri:
+            result["uri"] = self.uri
+        if self.mimeType:
+            result["mimeType"] = self.mimeType
+        if self.bufferView:
+            result["bufferView"] = self.bufferView
+        if self.extras:
+            result["extras"] = self.extras
+        return result
+
+
+class Material(Object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        self.key  = -1
+        self.pbrMetallicRoughness = kwargs.get("pbrMetallicRoughness")
+        self.normalTexture = kwargs.get("normalTexture")
+        self.occlusionTexture = kwargs.get("occlusionTexture")
+        self.emissiveTexture = kwargs.get("emissiveTexture")
+        self.emissiveFactor = kwargs.get("emissiveFactor")
+        self.alphaMode = kwargs.get("alphaMode")
+        self.alphaCutoff = kwargs.get("alphaCutoff")
+        self.doubleSided = kwargs.get("doubleSided")
+    def togltf(self):
+        result = super().togltf()
+        if self.pbrMetallicRoughness:
+            result["pbrMetallicRoughness"] = self.pbrMetallicRoughness.togltf()
+        if self.normalTexture:
+            result["normalTexture"] = self.normalTexture.togltf()
+        if self.occlusionTexture:
+            result["occlusionTexture"] = self.occlusionTexture.togltf()
+        if self.emissiveTexture:
+            result["emissiveTexture"] = self.emissiveTexture.togltf()
+        if self.alphaMode:
+            result["alphaMode"] = self.alphaMode.value
+        if self.alphaCutoff:
+            result["alphaCutoff"] = self.alphaCutoff
+        if self.doubleSided:
+            result["doubleSided"] = self.doubleSided
+        if self.extensions:
+            result["extensions"] = self.extensions
+        if self.extras:
+            result["extras"] = self.extras
+        return result
+
+
+class PBRMetallicRoughness(object):
+    def __init__(self, *args, **kwargs):
+        self.baseColorFactor = kwargs.get("baseColorFactor")
+        self.baseColorTexture = kwargs.get("baseColorTexture")
+        self.metallicFactor = kwargs.get("metallicFactor")
+        self.roughnessFactor = kwargs.get("roughnessFactor")
+        self.metallicRoughnessTexture = kwargs.get("metallicRoughnessTexture")
+        self.extensions = kwargs.get("extensions")
+        self.extras = kwargs.get("extras")
+    def togltf(self):
+        result = {}
+        if self.baseColorFactor:
+            result["baseColorFactor"] = self.baseColorFactor
+        if self.baseColorTexture:
+            result["baseColorTexture"] = self.baseColorTexture.togltf()
+        if self.metallicFactor:
+            result["metallicFactor"] = self.metallicFactor
+        if self.roughnessFactor:
+            result["roughnessFactor"] = self.roughnessFactor
+        if self.metallicRoughnessTexture:
+            result["metallicRoughnessTexture"] = self.metallicRoughnessTexture.togltf()
+        if self.extensions:
+            result["extensions"] = self.extensions
+        if self.extras:
+            result["extras"] = self.extras
+        return result
+
+Material.PBRMetallicRoughness = PBRMetallicRoughness
+
+class Mesh(Object):
+    def __init__(self, primitives, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        self.key  = -1
+        self.primitives = [] if primitives is None else primitives
+        self.weights = kwargs.get('weights')
+    def togltf(self):
+        result = super().togltf()
+        result["primitives"] = [primitive.togltf() for primitive in self.primitives]
+        if self.weights:
+            result["weights"] =  self.weights
+        return result
+
+
+class Primitive(object):
+    def __init__(self, attributes, indices, material, mode=PrimitiveMode.TRIANGLES, **kwargs):
+        self.attributes = attributes
+        self.indices = indices
+        self.material = material
+        self.mode = mode
+        self.targets = kwargs.get("targets")
+        self.extensions = kwargs.get("extensions")
+        self.extras = kwargs.get("extras")
+    def togltf(self):
+        result = {
+            "attributes": {key.value: attribute.key for key, attribute in self.attributes.items()},
+        }
+        if self.indices:
+            result["indices"] = self.indices.key
+        if self.material:
+            result["material"] = self.material.key
+        if self.mode:
+            result["mode"] = self.mode.value
+        if self.targets:
+            result["targets"] = self.targets
+        if self.extensions:
+            result["extensions"] = self.extensions
+        if self.extras:
+            result["extras"] = self.extras
+        return result
+
+Mesh.Primitive = Primitive
+
 
 class Node(Object):
     def __init__(self, *args, **kwargs):
@@ -277,124 +525,76 @@ class Node(Object):
             result["weights"] = self.weights
         return result
 
-class Accessor(Object):
-    def __init__(self, bufferView, byteOffset=None, count=0, type=AccessorType.SCALAR, componentType=ComponentType.FLOAT, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.key  = -1
-        self.bufferView = bufferView
-        self.byteOffset = byteOffset
-        self.componentType = componentType
-        self.normalized = False
-        self.count = count
-        self.type = type
-        self.max = None
-        self.min = None
-    def togltf(self):
-        result = super().togltf()
-        result["bufferView"] = self.bufferView.key
-        result["componentType"] = self.componentType.value
-        result["count"] = self.count
-        result["type"] = self.type.value
-        if self.bufferView:
-            result["bufferView"] = self.bufferView.key
-        if self.byteOffset is not None:
-            result["byteOffset"] = self.byteOffset
-        if self.max:
-            result["max"] = self.max
-        if self.min:
-            result["min"] = self.min
-        return result
 
-class BufferView(Object):
-    def __init__(self, buffer, byteOffset=None, byteLength=0, byteStride=None, target=BufferTarget.ARRAY_BUFFER, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.key    = -1
-        self.buffer = buffer
-        self.byteOffset = byteOffset
-        self.byteLength = byteLength
-        self.byteStride = byteStride
-        self.target = target
-    def togltf(self):
-        result = super().togltf()
-        result["buffer"] = self.buffer.key
-        result["byteLength"] = self.byteLength
-        if self.byteOffset is not None:
-            result["byteOffset"] = self.byteOffset
-        if self.byteStride is not None:
-            result["byteStride"] = self.byteStride
-        if self.target:
-            result["target"] = self.target.value
-        return result
-
-class Buffer(Object):
-    def __init__(self, byteLength=0, uri=None, *args, **kwargs):
+#TODO
+class Sampler(Object):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) 
         self.key   = -1
-        self.byteLength = byteLength
-        self.uri = uri
-    def togltf(self):
-        result = super().togltf()
-        result["byteLength"] = self.byteLength
-        if self.uri:
-            result["uri"] =  self.uri
-        return result
-
-class Primitive(object):
-    def __init__(self, attributes, indices, material, mode=PrimitiveMode.TRIANGLES, **kwargs):
-        self.attributes = attributes
-        self.indices = indices
-        self.material = material
-        self.mode = mode
-        self.targets = kwargs.get("targets")
-        self.extensions = kwargs.get("extensions")
+        self.magFilter = kwargs.get('magFilter')
+        self.minFilter = kwargs.get('minFilter')
+        self.wrapS = kwargs.get('wrapS')
+        self.wrapT = kwargs.get('wrapT')
         self.extras = kwargs.get("extras")
     def togltf(self):
-        result = {
-            "attributes": {key.value: attribute.key for key, attribute in self.attributes.items()},
-        }
-        if self.indices:
-            result["indices"] = self.indices.key
-        if self.material:
-            result["material"] = self.material.key
-        if self.mode:
-            result["mode"] = self.mode.value
-        if self.targets:
-            result["targets"] = self.targets
-        if self.extensions:
-            result["extensions"] = self.extensions
+        result = super().togltf()
+        if self.magFilter:
+            result["magFilter"] = self.magFilter.value
+        if self.minFilter:
+            result["minFilter"] = self.minFilter.value
+        if self.wrapS:
+            result["wrapS"] = self.wrapS.value
+        if self.wrapT:
+            result["wrapT"] = self.wrapT.value
         if self.extras:
             result["extras"] = self.extras
         return result
 
-class Mesh(Object):
-    def __init__(self, primitives, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
-        self.key  = -1
-        self.primitives = [] if primitives is None else primitives
-        self.weights = kwargs.get('weights')
-    def togltf(self):
-        result = super().togltf()
-        result["primitives"] = [primitive.togltf() for primitive in self.primitives]
-        if self.weights:
-            result["weights"] =  self.weights
-        return result
 
-class Material(Object):
+class Scene(Object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) 
-        self.key  = -1
-        self.values = {
-            "ambient": [0.0, 0.0, 0.0, 1],
-            "diffuse": [0.0, 0.0, 0.0, 1],
-            "specular": [0.0, 0.0, 0.0, 1],
-            "emission": [0.0, 0.0, 0.0, 1],
-            "shininess": 256
-        }
-    def __setitem__(self, key, item):
-        self.values[key] = item
-    def __getitem__(self, key):
-        return self.values[key]
+        self.key   = -1
+        self.nodes = kwargs.get('nodes', [])
     def togltf(self):
         result = super().togltf()
-        result["values"] = self.values
+        result["nodes"] = [node.key for node in self.nodes]
+        return result
+
+
+#TODO
+class Skin(Object):
+    pass
+
+
+class Texture(Object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        self.sampler = kwargs.get('sampler')
+        self.source = kwargs.get('source')
+        self.extras = kwargs.get("extras")
+    def togltf(self):
+        result = super().togltf()
+        if self.sampler:
+            result["sampler"] = self.sampler.key
+        if self.source:
+            result["source"] = self.source.key
+        if self.extras:
+            result["extras"] = self.extras
+        return result
+
+
+class TextureInfo(Object):
+    def __init__(self, index, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.index = index
+        self.texCoord = kwargs.get("texCoord")
+        self.extras = kwargs.get("extras")
+    def togltf(self):
+        result = super().togltf()
+        result["index"] = self.index.key
+        if self.texCoord:
+            result["texCoord"] = self.texCoord
+        if self.extras:
+            result["extras"] = self.extras
         return result
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'pygltf',
-    version = '0.1.0',
+    version = '0.2.0',
     license = 'MIT',
     author="Daniel Filonik",
     author_email = 'daniel.filonik@qut.edu.au',
